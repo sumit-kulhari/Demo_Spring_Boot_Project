082e671e3d3a6692bee5700bd795cddb74bd0070
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-08 11:57:21 +0530

    Updated Controller

--- src/main/java/com/markets/controller/EmployeeController.java
+++ src/main/java/com/markets/controller/EmployeeController.java
@@ -26,10 +26,6 @@ public class EmployeeController {
         return "Employee added";
     }
 
-//    @DeleteMapping("/delete")
-//    public void deleteEmployee(@RequestBody EmployeeEntity employee){
-//        employeeService.deleteEmploye(employee);
-//    }
     @DeleteMapping("/delete/{eid}")
     public String deleteEmployee(@PathVariable("eid") Integer eid){
         employeeService.deleteEmploye(eid);
