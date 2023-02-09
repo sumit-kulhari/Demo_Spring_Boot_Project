ca3b5503497eeb899e1be3dcbb6f1dd603d54d5f
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-09 14:46:10 +0530

    Updated Controller

--- src/main/java/com/markets/controller/EmployeeController.java
+++ src/main/java/com/markets/controller/EmployeeController.java
@@ -24,24 +24,24 @@ public class EmployeeController {
     public ResponseEntity<EmployeeEntity> saveEmployee(@RequestBody EmployeeEntity employee) {
         // save employee added
         EmployeeEntity employee1 = employeeService.saveEmployee(employee);
-        return new ResponseEntity<EmployeeEntity>(employee1,new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(employee1, new HttpHeaders(), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{eid}")
     public ResponseEntity<EmployeeEntity> deleteEmployee(@PathVariable("eid") Integer eid){
         EmployeeEntity employee = employeeService.deleteEmployee(eid);
-        return new ResponseEntity<EmployeeEntity>(employee,new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(employee, new HttpHeaders(), HttpStatus.OK);
     }
 
     @GetMapping( "/getbyID/{eid}")
     public ResponseEntity<EmployeeEntity> searchEmployee(@PathVariable("eid") Integer eid){
         EmployeeEntity employee = employeeService.searchEmployee(eid);
-        return new ResponseEntity<EmployeeEntity>(employee,new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(employee, new HttpHeaders(), HttpStatus.OK);
     }
 
     @PutMapping("/updateEmployee")
     public ResponseEntity<EmployeeEntity> updateEmployee(@RequestBody EmployeeEntity employee){
         EmployeeEntity employee1 = employeeService.updateEmployee(employee);
-        return new ResponseEntity<EmployeeEntity>(employee1,new HttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(employee1, new HttpHeaders(), HttpStatus.OK);
     }
 }
