8a7414890f9944f8dffafe92f0b93509f9a72e68
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-14 16:40:38 +0530

    Added Ratelimitter

--- src/main/java/com/markets/controller/EmployeeController.java
+++ src/main/java/com/markets/controller/EmployeeController.java
@@ -2,10 +2,15 @@ package com.markets.controller;
 
 import com.markets.entity.EmployeeEntity;
 import com.markets.service.EmployeeService;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +24,12 @@ import java.util.List;
 public class EmployeeController {
 
     private EmployeeService employeeService;
+//    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     @Autowired
     private DiscoveryClient discoveryClient;
+
+    @RateLimiter(name = "getMessageRateLimit", fallbackMethod = "getMessageFallBack")
     @GetMapping("/getall")
     public List<EmployeeEntity> displayall(){
         return employeeService.getAllEmployees();
@@ -56,5 +64,13 @@ public class EmployeeController {
     public List<ServiceInstance> serviceInstancesByApplicationName(
             @PathVariable String MarketsApplication) {
         return this.discoveryClient.getInstances(MarketsApplication);
+    }
+
+    @ExceptionHandler({ RequestNotPermitted.class })
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+    public ResponseEntity<String> getMessageFallBack(RequestNotPermitted exception) {
+
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                .body("Too many requests : No further request will be accepted. Please try after sometime");
     }
 }
